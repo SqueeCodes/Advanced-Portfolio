feb39cf33a901e03614cca849fa89b3a249a102c
David <itsmesquee@gmail.com>
2024-06-24 17:29:48 -0600

    working on modal pop-up

---
+++ app.js
@@ -0,0 +1,7 @@
+<figure class="modal__language">
+<img class="modal__language--img" src="./assets/python.jpg" alt="">
+</figure>
+<figure class="modal__language">
+<img  class="modal__language--img"src="./assets/fire base.jpg" alt="">
+</figure>
+</div>
